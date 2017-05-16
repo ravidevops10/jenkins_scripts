73f6b7d71b025f2be5140c98573ca23c701ea90b
newbie <newbie@joker.com>
2017-05-16 11:17:58 +0800

    增加过滤.svn的代码

--- generate_static_data.py
+++ generate_static_data.py
@@ -139,7 +139,8 @@ def scan_files():
     ##.decode(DEFAULT_ENCODING))\
     """
     return [os.path.join(EXCEL_CONFIG_PATH, x)\
-             for x in os.listdir("./" + EXCEL_CONFIG_PATH)]
+             for x in os.listdir("./" + EXCEL_CONFIG_PATH)\
+                if x.strip() != ".svn"]
 
 def write_to_text(_name, _data):
     """
