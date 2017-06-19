043194aaf528eda498b388a5c3f5020a0c5d6cef
chengjie <173855403@qq.com>
2017-06-19 17:30:57 +0800

    ceshi

--- test_live2d_res.py
+++ test_live2d_res.py
@@ -46,7 +46,7 @@ class Live2dResources(object):
         clothes = []
         for role in role_ids:
             clothes.extend(Live2dResources.get_l2d_settings(role))
-            print clothes
+            print "clothes in loop is ", clothes
         
         print "current path is ", os.getcwd()
         print "here we got :", os.listdir(LIVE2D_RES_PATH)
