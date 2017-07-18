daae6c66920f4e433d816dbf10937e7c925ee816
chengjie <173855403@qq.com>
2017-07-18 11:59:38 +0800

    修正utf8问题

--- generate_static_data.py
+++ generate_static_data.py
@@ -149,8 +149,10 @@ def write_to_text(_name, _data):
     """
     write data to text file.
     """
+    #import codecs
+    #with codecs.open()
 
-    with open(WRITE_TO_FILE % _name, "wb" ) as _f:
+    with open(WRITE_TO_FILE % _name, "w" ) as _f:
         wd = "\n".join(_data)
         _f.write(wd.encode("utf8"))
 
